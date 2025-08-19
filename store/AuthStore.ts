9be6b9adacb0b173b347fe0a1d30a5034b796f64
isaacmatovu <matovuisaac646@gmail.com>
2025-08-19 12:42:43 +0300

    added zustand store

---
+++ store/AuthStore.ts
@@ -0,0 +1,101 @@
+import { account } from "@/lib/appwrite";
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+interface AuthState {
+  isLoggedIn: boolean;
+  user: User | null;
+  isLoading: boolean;
+  error: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string, name: string) => Promise<void>;
+  logOut: () => Promise<void>;
+  checkSession: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  //initial state values
+  isLoggedIn: false,
+  user: null,
+  isLoading: true,
+  error: null,
+
+  //login function
+  login: async (email, password) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      await account.createEmailPasswordSession(email, password);
+      //get user data
+      const user = await account.get();
+      //update state
+      set({
+        isLoggedIn: true,
+        user: { id: user.$id, name: user.name, email: user.email },
+        isLoading: false,
+      });
+    } catch (error) {
+      if (error instanceof Error)
+        set({ error: error.message, isLoading: false }); //show error
+    }
+  },
+
+  register: async (email, password, name) => {
+    set({ isLoading: true, error: null });
+
+    try {
+      //create account
+      await account.create(email, password, name);
+
+      //log in user automatically
+      await account.createEmailPasswordSession(email, password);
+      //get user data
+      const user = await account.get();
+
+      //update state
+      set({
+        isLoggedIn: true,
+        user: {
+          id: user.$id,
+          name: user.name,
+          email: user.email,
+        },
+        isLoading: false,
+      });
+    } catch (error) {
+      if (error instanceof Error)
+        set({ error: error.message, isLoading: false });
+    }
+  },
+
+  logOut: async () => {
+    try {
+      await account.deleteSession("current"); //clear current session
+      set({ isLoggedIn: false, user: null }); //reset state
+    } catch (error) {
+      if (error instanceof Error) set({ error: error.message });
+    }
+  },
+
+  //checking session
+  checkSession: async () => {
+    try {
+      const user = await account.get(); //try to get user data
+      set({
+        isLoggedIn: true,
+        user: { id: user.$id, name: user.name, email: user.email },
+        isLoading: false,
+      });
+    } catch (error) {
+      set({ isLoading: false });
+    }
+  },
+}));
+
+//check session on initial load
+useAuthStore.getState().checkSession();
