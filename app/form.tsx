d819d74be6308ac8fe89ad1adb8c42596e6a44ec
isaacmatovu <matovuisaac646@gmail.com>
2025-08-28 19:53:57 +0300

    added form validation

--- app/form.tsx
+++ app/form.tsx
@@ -20,6 +20,12 @@ interface FormData {
   email: string;
   password: string;
 }
+interface Errors {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
 
 const Form = () => {
   const [form, setForm] = useState<FormData>({
@@ -30,6 +36,12 @@ const Form = () => {
   });
   const { login, register, isLoading, error, isLoggedIn } = useAuthStore();
   const [hasAccount, setHasAccount] = useState(false);
+  const [errors, setErrors] = useState<Errors>({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
 
   const router = useRouter();
 
@@ -46,10 +58,36 @@ const Form = () => {
   const handleRegister = () => {
     setHasAccount(!hasAccount);
   };
+
+  const validate = () => {
+    const Error = { firstname: "", lastname: "", email: "", password: "" };
+    if (!form.firstname) {
+      Error.firstname = "Please enter your firstname";
+    } else if (!form.lastname) {
+      Error.lastname = "Please enter your lastname";
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!form.email) {
+      Error.email = "Enter your email address";
+    } else if (!emailRegex.test(form.email)) {
+      Error.email = "Enter a valid email";
+    }
+
+    if (!form.password) {
+      Error.password = "Enter your password";
+    } else if (form.password.length < 8) {
+      Error.password = "Password should be atleast 8 characters long";
+    }
+    return Error;
+  };
   const handleSubmit = () => {
+    const ErrorForm = validate();
     if (hasAccount) {
+      setErrors(ErrorForm);
       register(form.email, form.password, `${form.firstname} ${form.lastname}`);
     } else {
+      setErrors(ErrorForm);
       login(form.email, form.password);
     }
   };
@@ -69,46 +107,62 @@ const Form = () => {
                   Join Us to make this wounderful journey
                 </Text>
                 <View className="mb-6">
-                  <TextInput
-                    style={styles.text}
-                    label="Enter your first name"
-                    mode="outlined"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, firstname: text }))
-                    }
-                  />
-                  <TextInput
-                    style={styles.text}
-                    label="Enter your Last name"
-                    mode="outlined"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, lastname: text }))
-                    }
-                  />
-                  <TextInput
-                    style={styles.text}
-                    autoCapitalize="none"
-                    label="Email"
-                    placeholder="@examplegmail.com"
-                    keyboardType="email-address"
-                    mode="outlined"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, email: text }))
-                    }
-                  />
-                  <TextInput
-                    style={styles.text}
-                    label="Enter password"
-                    mode="outlined"
-                    autoCapitalize="none"
-                    secureTextEntry
-                    placeholder="password"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, password: text }))
-                    }
-                  />
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      label="Enter your first name"
+                      mode="outlined"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, firstname: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.firstname}</Text>
+                  </View>
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      label="Enter your Last name"
+                      mode="outlined"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, lastname: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.lastname}</Text>
+                  </View>
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      autoCapitalize="none"
+                      label="Email"
+                      placeholder="@examplegmail.com"
+                      keyboardType="email-address"
+                      mode="outlined"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, email: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.email}</Text>
+                  </View>
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      label="Enter password"
+                      mode="outlined"
+                      autoCapitalize="none"
+                      secureTextEntry
+                      placeholder="password"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, password: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.password}</Text>
+                  </View>
                 </View>
-                <Button mode="contained" onPress={handleSubmit}>
+                <Button
+                  disabled={!validate()}
+                  mode="contained"
+                  onPress={handleSubmit}
+                >
                   Sign Up
                 </Button>
                 <View className="flex-col justify-center items-center">
@@ -138,30 +192,36 @@ const Form = () => {
                   <Text className="text-white text-4xl text-center">
                     Sign In
                   </Text>
-                  <TextInput
-                    style={styles.text}
-                    activeOutlineColor="green"
-                    autoCapitalize="none"
-                    label="Email"
-                    placeholder="@examplegmail.com"
-                    keyboardType="email-address"
-                    mode="outlined"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, email: text }))
-                    }
-                  />
-                  <TextInput
-                    style={styles.text}
-                    activeOutlineColor="green"
-                    label="Enter password"
-                    autoCapitalize="none"
-                    secureTextEntry
-                    placeholder="password"
-                    mode="outlined"
-                    onChangeText={(text) =>
-                      setForm((prev) => ({ ...prev, password: text }))
-                    }
-                  />
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      activeOutlineColor="green"
+                      autoCapitalize="none"
+                      label="Email"
+                      placeholder="@examplegmail.com"
+                      keyboardType="email-address"
+                      mode="outlined"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, email: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.email}</Text>
+                  </View>
+                  <View className="flex flex-row">
+                    <TextInput
+                      style={styles.text}
+                      activeOutlineColor="green"
+                      label="Enter password"
+                      autoCapitalize="none"
+                      secureTextEntry
+                      placeholder="password"
+                      mode="outlined"
+                      onChangeText={(text) =>
+                        setForm((prev) => ({ ...prev, password: text }))
+                      }
+                    />
+                    <Text className="text-red-500">{errors.password}</Text>
+                  </View>
                 </View>
                 <Button mode="contained" onPress={handleSubmit}>
                   Sign In
