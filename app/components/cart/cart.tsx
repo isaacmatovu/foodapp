7e196cc991dd7f04fd20877822d988c04bd67769
isaacmatovu <matovuisaac646@gmail.com>
2025-09-04 15:52:28 +0300

    added make order button and functionality

--- app/components/cart/cart.tsx
+++ app/components/cart/cart.tsx
@@ -1,13 +1,89 @@
+import { databases, ID } from "@/lib/appwrite";
+import { useAuthStore } from "@/store/AuthStore";
 import UseCartStore from "@/store/CartStore";
+import Entypo from "@expo/vector-icons/Entypo";
 import EvilIcons from "@expo/vector-icons/EvilIcons";
-import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import {
+  Image,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import { Button, TextInput } from "react-native-paper";
 
+interface Error {
+  number: string;
+}
 const Cart = () => {
+  const [NumberInput, setNumberInput] = useState<number>(0);
+  const [error, setErrors] = useState<Error>({
+    number: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [productError, setProductError] = useState("");
+  const [ViewPrice, setViewPrcie] = useState(false);
   const { items, UpdateQuantity, removeFromCart, getTotalPrice, clearCart } =
     UseCartStore();
+  const { user } = useAuthStore();
   const totalPrice = getTotalPrice();
+  const router = useRouter();
+
+  const handleCart = async () => {
+    const itemsString = JSON.stringify(
+      items.map((item) => ({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        quantity: item.quantity,
+      }))
+    );
+    try {
+      setLoading(true);
+      const orderData = {
+        userId: user?.id,
+        Username: user?.name,
+        tableNumber: NumberInput,
+        items: itemsString,
+        total: totalPrice,
+        createdAt: new Date().toISOString(),
+      };
+      await databases.createDocument(
+        "68b96e8a000fd3222348",
+        "orders_table",
+        ID.unique(),
+        orderData
+      );
+      router.replace("/(tabs)");
+      clearCart();
+    } catch {
+      setProductError("An error occured while sending to the database");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleValidation = () => {
+    const Error: Error = { number: "" };
+    if (NumberInput === 0 || NumberInput < 0) {
+      Error.number = "Enter a number more than 0 ";
+    } else if (!NumberInput) {
+      Error.number = "Enter table number";
+    }
+    setErrors(Error);
+  };
+
+  const handlesubmit = () => {
+    handleValidation();
+  };
+
   return (
     <View>
       {items.length === 0 ? (
@@ -73,13 +149,49 @@ const Cart = () => {
             </View>
           ))}
           <View className="flex flex-row justify-between items-center">
-            <Text className="text-xl font-semibold">
-              Total Price : {totalPrice.toFixed(2)}
-            </Text>
+            <View className="flex">
+              <Text className="text-xl font-semibold">
+                <TouchableOpacity onPress={() => setViewPrcie(!ViewPrice)}>
+                  <Entypo
+                    name={ViewPrice ? "eye-with-line" : "eye"}
+                    size={24}
+                    color="black"
+                  />
+                </TouchableOpacity>
+                Total Price : {ViewPrice ? "******" : totalPrice.toFixed(2)}
+              </Text>
+            </View>
             <Button style={styles.button} onPress={clearCart} mode="contained">
               Clear cart
             </Button>
           </View>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            className="flex-1"
+          >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+              <TextInput
+                keyboardType="number-pad"
+                placeholder="Enter table number"
+                onChangeText={(text) => setNumberInput(Number(text))}
+                style={styles.input}
+              />
+            </TouchableWithoutFeedback>
+          </KeyboardAvoidingView>
+
+          {error && <Text className="text-red-500 mt-3">{error.number}</Text>}
+          {productError && (
+            <Text className="text-red-500 mt-3">{productError}</Text>
+          )}
+          <Button
+            disabled={NumberInput === 0 || loading}
+            mode="contained"
+            onPress={() => {
+              (handlesubmit(), handleCart());
+            }}
+          >
+            Make order
+          </Button>
         </ScrollView>
       )}
     </View>
@@ -95,6 +207,9 @@ const styles = StyleSheet.create({
   },
   button: {
     width: 200,
+  },
+  input: {
+    width: 80,
   },
 });
 export default Cart;
