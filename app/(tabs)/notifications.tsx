2399fa825d1936f91f59e42c1ce4c56073e82d7c
isaacmatovu <matovuisaac646@gmail.com>
2025-09-04 15:53:46 +0300

    profile section

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import Profile from "../components/profile/profile";
 
 const Notifications = () => {
   return (
     <View>
-      <Text>Notifications</Text>
+      <Profile />
     </View>
   );
 };
