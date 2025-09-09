f82e8e8a7dc86c891e2fd58555c5e830b49617d6
isaacmatovu <matovuisaac646@gmail.com>
2025-09-09 15:55:31 +0300

    refactored to reusable component

--- app/components/admin/admin.tsx
+++ app/components/admin/admin.tsx
@@ -1,6 +1,13 @@
 import { databases } from "@/lib/appwrite";
+import { useAuthStore } from "@/store/AuthStore";
 import React, { useEffect, useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 interface Orders {
   $id: string;
   userId: string;
@@ -21,10 +28,12 @@ const Admin = () => {
   const [orders, setOrders] = useState<Orders[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const { user } = useAuthStore();
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
+        setLoading(true);
         const response = await databases.listDocuments(
           "68b96e8a000fd3222348",
           "orders_table"
@@ -32,6 +41,8 @@ const Admin = () => {
         setOrders(response.documents as unknown as Orders[]);
       } catch {
         setError("Failed to fetch orders check your internet connection");
+      } finally {
+        setLoading(false);
       }
     };
     fetchOrders();
@@ -39,18 +50,30 @@ const Admin = () => {
 
   return (
     <ScrollView className="flex-1 p-4">
-      <Text className="text-2xl font-bold mb-4">Orders</Text>
+      <View className="bg-gray-600 flex justify-center items-center mt-6 ml-4 mr-4 border-2 border-r-yellow-500 border-l-yellow-500 rounded-xl border-t-0 border-b-0">
+        <View className="flex flex-row gap-5">
+          <Text className="text-green-300 text-xl">Welcome</Text>
+          <Text className="text-white text-xl">{user?.name}</Text>
+        </View>
+      </View>
+      <View>
+        <Text className="font-bold text-xl">Orders</Text>
+      </View>
       <View>
         {loading ? (
-          <Text>Loading...</Text>
+          <ActivityIndicator size={30} color={"green"} />
+        ) : error ? (
+          <Text className="text-red-500 text-xl">{error}</Text>
         ) : (
-          <View>
+          <View className="w-full h-full bg-[#1F1C2C]">
             {orders.map((order) => {
               //pass the items  string back to an array of objects
               const orderItems: OrderItem[] = JSON.parse(order.items);
               return (
                 <View key={order.$id} style={styles.orderCard}>
-                  <Text>Customer:{order.Username}</Text>
+                  <Text className="font-bold text-green-500">
+                    Customer:{order.Username}
+                  </Text>
                   <Text>Table:{order.tableNumber}</Text>
                   <Text>Date:{new Date(order.createdAt).toLocaleString()}</Text>
                   <Text>Total amount:Ugsh.{order.total}</Text>
