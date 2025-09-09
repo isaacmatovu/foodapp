d3bb3d7e581486725313cd12232842f6b4b20cbf
isaacmatovu <matovuisaac646@gmail.com>
2025-09-09 15:56:33 +0300

    created reusable components

--- app/components/home/category.tsx
+++ app/components/home/category.tsx
@@ -1,7 +1,7 @@
-import categories from "@/data/data";
-import React, { useState } from "react";
-import { FlatList, Image, ScrollView, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import React, { useCallback, useMemo, useState } from "react";
+import { View } from "react-native";
+import CategorySelection from "./categorySelection";
+import CategorySelectionDisplay from "./CategorySelectionDisplay";
 
 interface Categoty {
   name: string;
@@ -10,66 +10,24 @@ interface Categoty {
 const Category = () => {
   const [selectedCategory, setSelectedCategory] = useState<string>("Junk Food");
 
-  //find the current selected object
-  const currentCategory = categories.find(
-    (cat) => cat.name === selectedCategory
+  const handleSelectedCategory = useCallback((className: string) => {
+    setSelectedCategory(className);
+    console.log("item selected");
+  }, []);
+
+  const Child1 = useMemo(
+    () => <CategorySelection handleSelectedCategory={handleSelectedCategory} />,
+    []
+  );
+  const Child2 = useMemo(
+    () => <CategorySelectionDisplay selectedCategory={selectedCategory} />,
+    [selectedCategory]
   );
   return (
     <View>
-      <View className="mt-7">
-        <Text className="text-white font-bold text-xl mb-7">
-          Sushi Category
-        </Text>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          className="py-2"
-        >
-          <View className="flex flex-row gap-x-4">
-            {categories.map((category) => (
-              <View key={category.name} className="flex-col mb-3">
-                <View className="bg-[#06D6A0] p-2 rounded-3xl w-32">
-                  <Button
-                    onPress={() => {
-                      setSelectedCategory(category.name);
-                    }}
-                    className="text-white text-xl text-center"
-                  >
-                    {category.name}
-                  </Button>
-                </View>
-              </View>
-            ))}
-          </View>
-        </ScrollView>
-      </View>
+      {Child1}
       {/* subcategories */}
-      <View>
-        <Text className="text-white text-2xl">{selectedCategory}</Text>
-
-        {currentCategory && (
-          <FlatList
-            data={currentCategory.subcategories}
-            keyExtractor={(item, index) => index.toString()}
-            numColumns={2}
-            columnWrapperStyle={{ justifyContent: "space-between" }}
-            showsVerticalScrollIndicator={false}
-            renderItem={({ item }) => (
-              <View className="bg-white rounded-2xl p-4 mb-4 w-[48%] shadow-sm">
-                <View className="items-center mb-3">
-                  <Image source={item.image} className="w-full h-24" />
-                </View>
-                <Text className="text-lg font-semibold text-gray-800 text-center mb-1">
-                  {item.name}
-                </Text>
-                <Text className="text-green-600 font-bold text-center">
-                  ${item.price.toFixed(2)}
-                </Text>
-              </View>
-            )}
-          />
-        )}
-      </View>
+      {Child2}
     </View>
   );
 };
