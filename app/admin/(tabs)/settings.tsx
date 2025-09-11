53b2933af436019af67be4bcae7508a0149ced79
isaacmatovu <matovuisaac646@gmail.com>
2025-09-11 11:07:58 +0300

    used reusable component

--- app/admin/(tabs)/settings.tsx
+++ app/admin/(tabs)/settings.tsx
@@ -1,48 +1,13 @@
-import { useAuthStore } from "@/store/AuthStore";
-import Feather from "@expo/vector-icons/Feather";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Settings from "@/app/components/common/settings";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
-const Settings = () => {
-  const { logOut } = useAuthStore();
+const AdminSettings = () => {
   return (
-    <View className="flex-1 bg-black">
-      <View>
-        <Text className="text-white font-medium text-2xl mb-20">
-          Sushi food!
-        </Text>
-        <View className="flex flex-row gap-6 mb-12">
-          <FontAwesome5 name="grin-wink" size={24} color="white" />
-          <Text className="text-white text-xl">Beneficiaries</Text>
-        </View>
-        <View className="flex flex-row gap-6 mb-12">
-          <FontAwesome name="address-book-o" size={24} color="white" />
-          <Text className="text-white text-xl">More information</Text>
-        </View>
-        <View className="flex flex-row gap-6 mb-12">
-          <MaterialIcons name="support-agent" size={24} color="white" />
-          <Text className="text-white text-xl">Support</Text>
-        </View>
-        <View className="flex flex-row gap-6 mb-12">
-          <Feather name="settings" size={24} color="white" />
-          <Text className="text-white text-xl">Settings</Text>
-        </View>
-        <View className="flex flex-row gap-6 mb-12">
-          <FontAwesome5 name="receipt" size={24} color="white" />
-          <Text className="text-white text-xl">Get Reciept</Text>
-        </View>
-        <View className="flex flex-row gap-6 mb-12">
-          <MaterialIcons name="logout" size={24} color="white" />
-          <Text className="text-white text-xl" onPress={logOut}>
-            Logout
-          </Text>
-        </View>
-      </View>
+    <View>
+      <Settings />
     </View>
   );
 };
 
-export default Settings;
+export default AdminSettings;
